816eb1eb00bc1810cf204410682da4f2819adbd9
sunny <sunnyroy8084401051@gmail.com>
2023-01-26 23:10:55 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+/*
+Question : For given array sort in ascending order by swapping method.
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = {0,0,0,1,0,1,1,1};
+        int temp;
+        while (arr[0] < arr[arr.length - 1]){
+            temp = arr[0];
+
+        }
+    }
+}
