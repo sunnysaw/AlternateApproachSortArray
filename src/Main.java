6cb2bc1c539d1ebefb80f18c411eb58afc676b25
sunny gupta <96872697+sunnysaw@users.noreply.github.com>
2023-01-27 21:05:15 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,31 @@ Question : For given array sort in ascending order by swapping method.
  */
 
 public class Main {
+    public static void SortArray(int[] arr){
+        int n = arr.length;
+        int temp , left = 0 , right = n -1;
+        while (left < right){
+            if (arr[left] == 1 && arr[right] == 0){
+                temp = arr[left];
+                arr[left] = arr[right];
+                arr[right] = temp;
+                left++;
+                right--;
+            }
+            if (arr[left] == 0){
+                left++;
+            }
+            if (arr[right] == 1){
+                right--;
+            }
+        }
+        System.out.println("NOW PRINTING THE FINAL RESULT :");
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i]);
+        }
+    }
     public static void main(String[] args) {
         int[] arr = {0,0,0,1,0,1,1,1};
-        int temp;
-        while (arr[0] < arr[arr.length - 1]){
-            temp = arr[0];
-
-        }
+        SortArray(arr);
     }
 }
